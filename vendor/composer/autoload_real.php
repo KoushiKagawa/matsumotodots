c9f74013c8da454beecd1193ed9fbe12f7012cc7
KoushiKagawa <koushi.kagawa@resinc.co.jp>
2017-06-22 16:23:07 +0900

    “gurunavi”

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit73d734cf0bc9506ff539dbf9e3c31331
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit73d734cf0bc9506ff539dbf9e3c31331::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire73d734cf0bc9506ff539dbf9e3c31331($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire73d734cf0bc9506ff539dbf9e3c31331($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
